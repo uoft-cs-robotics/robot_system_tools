f298302de5baad0bf515b41a4c8182681e69561a
Ruthrash <ruthrash.hari@gmail.com>
2023-03-30 14:47:00 -0400

    test cases for joint position and cartesian postion(square traj) controllers

---
+++ tests/frankapy_control_test_scripts/frankapy_ee_test.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+from autolab_core import RigidTransform
+from frankapy import FrankaArm, SensorDataMessageType
+from frankapy import FrankaConstants as FC
+from frankapy.proto_utils import sensor_proto2ros_msg, make_sensor_group_msg
+from frankapy.proto import PosePositionSensorMessage, ShouldTerminateSensorMessage, CartesianImpedanceSensorMessage
+from franka_interface_msgs.msg import SensorDataGroup
+
+from frankapy.utils import min_jerk, min_jerk_weight
+
+import rospy
+from tests_common import *
+from tests_common import SynchMessages
+import matplotlib.pyplot as plt
+import time
+
+if __name__ == "__main__":
+    fa = FrankaArm()
+    fa.reset_joints()
+    synch_messages_object = SynchMessages(commands_topic_name=FC.DEFAULT_SENSOR_PUBLISHER_TOPIC)
+
+    rospy.loginfo('Generating Trajectory')
+    p0 = fa.get_pose()
+
+    square_length = 0.1
+    dt = 0.001
+
+    X1 = p0.translation.copy()
+    X2 = p0.translation.copy()
+    X1[1] -= square_length/2.0
+    X2[1] += square_length/2.0
+    X3 = X2.copy() 
+    X3[2] -= square_length
+
+    X4 = X1.copy()
+    X4[2] -= square_length
+
+    waypoints = generate_square_pose_traj(X1, X2, X3, X4, velocity=0.1, 
+                                        square_length=square_length, dt=0.001)
+
+    p0.translation = waypoints[0]
+    rospy.loginfo('Initializing Sensor Publisher')
+    pub = rospy.Publisher(FC.DEFAULT_SENSOR_PUBLISHER_TOPIC, SensorDataGroup, queue_size=1000)
+    rate = rospy.Rate(1 / dt)
+
+    rospy.loginfo('Going to initial point...')
+    # To ensure skill doesn't end before completing trajectory, make the buffer time much longer than needed
+    fa.goto_pose(p0)
+    print(p0.translation, X1)
+    time.sleep(2.0)
+    rospy.loginfo('Starting test...')
+    init_time = rospy.Time.now().to_time()
+    for i in range(1, len(waypoints)):
+        timestamp = rospy.Time.now().to_time() - init_time
+        traj_gen_proto_msg = PosePositionSensorMessage(
+            id=i, timestamp=timestamp, 
+            position=waypoints[i], quaternion=p0.quaternion
+        )
+
+        ros_msg = make_sensor_group_msg(
+            trajectory_generator_sensor_msg=sensor_proto2ros_msg(
+                traj_gen_proto_msg, SensorDataMessageType.POSE_POSITION),
+        )
+        ros_msg.header.stamp = rospy.Time.now()       
+        pub.publish(ros_msg)
+        rate.sleep()
+
+    # Stop the skill
+    # Alternatively can call fa.stop_skill()
+    term_proto_msg = ShouldTerminateSensorMessage(timestamp=rospy.Time.now().to_time() - init_time, should_terminate=True)
+    ros_msg = make_sensor_group_msg(
+        termination_handler_sensor_msg=sensor_proto2ros_msg(
+            term_proto_msg, SensorDataMessageType.SHOULD_TERMINATE)
+        )
+    pub.publish(ros_msg)
+
+    rospy.loginfo('Done')
+
+    poses_cmd = []; poses_measured = []
+    d_poses_cmd = []; d_poses_measured = []
+    cmds, robot_states = synch_messages_object.get_synched_messages()
+    print(len(cmds), len(robot_states))
+    for cmd, robot_state in zip(cmds, robot_states):
+        deserialized_msg = PosePositionSensorMessage.FromString(cmd.trajectoryGeneratorSensorData.sensorData )
+        poses_cmd.append(deserialized_msg.position) 
+        poses_measured.append(np.array([robot_state.O_T_EE[12],
+                                robot_state.O_T_EE[13],
+                                robot_state.O_T_EE[14]]))
+    plot_ee_level(poses_measured, poses_cmd)
+        
+        
+
+                        
---
+++ tests/frankapy_control_test_scripts/test.py
@@ -0,0 +1,92 @@
+import numpy as np
+
+from autolab_core import RigidTransform
+from frankapy import FrankaArm, SensorDataMessageType
+from frankapy import FrankaConstants as FC
+from frankapy.proto_utils import sensor_proto2ros_msg, make_sensor_group_msg
+from frankapy.proto import PosePositionSensorMessage, ShouldTerminateSensorMessage, CartesianImpedanceSensorMessage
+from franka_interface_msgs.msg import SensorDataGroup
+
+from frankapy.utils import min_jerk, min_jerk_weight
+
+import rospy
+
+
+if __name__ == "__main__":
+    fa = FrankaArm()
+    fa.reset_joints()
+
+    rospy.loginfo('Generating Trajectory')
+    p0 = fa.get_pose()
+    p1 = p0.copy()
+    T_delta = RigidTransform(
+        translation=np.array([0, 0, 0.2]),
+        rotation=RigidTransform.z_axis_rotation(np.deg2rad(30)), 
+                            from_frame=p1.from_frame, to_frame=p1.from_frame)
+    p1 = p1 * T_delta
+    fa.goto_pose(p1)
+
+    T = 5
+    dt = 0.02
+    ts = np.arange(0, T, dt)
+    current_gripper_width = 0.08
+    has_closed = False
+
+    weights = [min_jerk_weight(t, T) for t in ts]
+    pose_traj = [p1.interpolate_with(p0, w) for w in weights]
+
+    z_stiffness_traj = [min_jerk(100, 800, t, T) for t in ts]
+
+    rospy.loginfo('Initializing Sensor Publisher')
+    pub = rospy.Publisher(FC.DEFAULT_SENSOR_PUBLISHER_TOPIC, SensorDataGroup, queue_size=1000)
+    rate = rospy.Rate(1 / dt)
+
+    rospy.loginfo('Publishing pose trajectory...')
+    # To ensure skill doesn't end before completing trajectory, make the buffer time much longer than needed
+    fa.goto_pose(pose_traj[1], duration=T, dynamic=True, buffer_time=10,
+        # cartesian_impedances=FC.DEFAULT_TRANSLATIONAL_STIFFNESSES[:2] + [z_stiffness_traj[1]] + FC.DEFAULT_ROTATIONAL_STIFFNESSES
+    )
+    init_time = rospy.Time.now().to_time()
+    for i in range(2, len(ts)):
+        timestamp = rospy.Time.now().to_time() - init_time
+        traj_gen_proto_msg = PosePositionSensorMessage(
+            id=i, timestamp=timestamp, 
+            position=pose_traj[i].translation, quaternion=pose_traj[i].quaternion
+        )
+        # fb_ctrlr_proto = CartesianImpedanceSensorMessage(
+        #     id=i, timestamp=timestamp,
+        #     translational_stiffnesses=FC.DEFAULT_TRANSLATIONAL_STIFFNESSES[:2] + [z_stiffness_traj[i]],
+        #     rotational_stiffnesses=FC.DEFAULT_ROTATIONAL_STIFFNESSES
+        # )
+        ros_msg = make_sensor_group_msg(
+            trajectory_generator_sensor_msg=sensor_proto2ros_msg(
+                traj_gen_proto_msg, SensorDataMessageType.POSE_POSITION)
+            # feedback_controller_sensor_msg=sensor_proto2ros_msg(
+            #     fb_ctrlr_proto, SensorDataMessageType.CARTESIAN_IMPEDANCE)
+            )
+
+        if not has_closed:
+            current_gripper_width -= 0.0005
+        else:
+            current_gripper_width += 0.0005
+
+        if current_gripper_width < 0.002:
+            has_closed = True
+
+        fa.goto_gripper(current_gripper_width, block=False)
+
+        
+        rospy.loginfo('Publishing: ID {}'.format(traj_gen_proto_msg.id))
+        pub.publish(ros_msg)
+        rate.sleep()
+
+    # Stop the skill
+    # Alternatively can call fa.stop_skill()
+    term_proto_msg = ShouldTerminateSensorMessage(timestamp=rospy.Time.now().to_time() - init_time, should_terminate=True)
+    ros_msg = make_sensor_group_msg(
+        termination_handler_sensor_msg=sensor_proto2ros_msg(
+            term_proto_msg, SensorDataMessageType.SHOULD_TERMINATE)
+        )
+    pub.publish(ros_msg)
+
+    rospy.loginfo('Done')
