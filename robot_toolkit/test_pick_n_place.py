4290983814da1dbc27c885d7832c3aa7cd26b77a
Ruthrash <ruthrash.hari@gmail.com>
2023-11-02 21:42:12 -0400

    modified test scripts to include pick and place with megapose

--- robot_toolkit/test_pick_n_place.py
+++ robot_toolkit/test_pick_n_place.py
@@ -7,26 +7,37 @@ from robot_arm_algos.src.pick_and_place.object import Object
 from robot_arm_algos.src.pick_and_place.pick_and_place import PickAndPlace
 from robot_arm_algos.src.config_reader import read_yaml_file
 from robot_arm_algos.src.logger import logger
+from robot_arm_algos.src.inference.megapose6d import MegaPose6D
 
 test_config = {
     "extrinsics_file" : "robot_arm_algos/robot_camera_extrinsics.yaml",
-    "grasps_file": "tests/data/grasps.yaml",
+    "grasps_file": "tests/data/grasps_powerdrill.yaml",
     "aruco_board_data": ArucoBoardData(dictionary="DICT_6X6_1000",
                                 marker_length = 0.0225,
                                 marker_separation = 0.0025,
                                 n_rows = 2,
                                 n_cols = 2,
-                                ids = [0,1,2,3]),      
+                                ids = [0,1,2,3]),  
+    "object_name" : "powerdrill",
+    "object_dir_path_base": "robot_arm_algos/src/inference/objects/",
     "camera_in_hand": True
     }
 
 
 def main():
-
+    obj_dir_path = test_config["object_dir_path_base"] + test_config["object_name"]
+    megapose = MegaPose6D(object_name = test_config["object_name"],
+                          obj_dir_path = obj_dir_path)
+    
     aruco_board_tag = ArucoBoard(test_config["aruco_board_data"])  
-    object_ = Object(object_name = "cube 4 with arucoboard tag",
-                    tag = aruco_board_tag)   
-    camera = RealSenseCamera(camera_id='145522066546')
+    # object_ = Object(object_name = "cube 4 with arucoboard tag",
+    #                 tag = aruco_board_tag)   
+    object_ = Object(object_name = test_config["object_name"],
+                    tag = None,
+                    pose_estimator = megapose)    
+    
+    
+    camera = RealSenseCamera()
     robot_arm_object = robot_arm_object = RobotFrankaPy(init_node = True, 
                                                         with_franka_gripper = True)   
     robot_arm_object.fpy_object.reset_joints()      
@@ -37,7 +48,7 @@ def main():
                               camera_in_hand = test_config["camera_in_hand"])
     # picknplace.grasp(object = object_,
     #                 grasps_file =  "tests/data/grasps.yaml")
-    grasp_pose = picknplace.pick_object(object = object_,
+    grasp_pose = picknplace.pick_object(object_ = object_,
                     grasps_file =  test_config["grasps_file"])
     grasp_pose[0,3] += 0.1    
     picknplace.place_object(place_pose = grasp_pose)
