505b7942c2bf7b1bd4352cdc19a4da0e9f5e9778
Ruthrash <ruthrash.hari@gmail.com>
2024-01-31 12:33:41 -0500

    added test scripts for bbox annotator and megapose

--- robot_toolkit/test_bbox_annotator.py
+++ robot_toolkit/test_bbox_annotator.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
 import cv2
 from robot_arm_algos.src.camera.realsense_camera import RealSenseCamera
-from robot_arm_algos.src.camera.camera import get_bbox_annotations, get_segmap_from_bbox
+from robot_arm_algos.src.camera.camera import get_bbox_annotations, get_segmap_from_bbox, get_segmap_from_bbox_with_depth
 
 def main(): 
     rs_camera = RealSenseCamera() 
-    image = rs_camera.get_current_rgb_frame() 
-    bbox, cropped_image = get_bbox_annotations(image)
-    image = cv2.rectangle(image, (bbox.xmin, bbox.ymax), (bbox.xmax, bbox.ymin), (0, 0, 0), 2) 
-    plt.imshow(image)
+    rgb_image, depth_image = rs_camera.get_current_rgbd_frames() 
+    bbox, cropped_image = get_bbox_annotations(rgb_image)
+    rgb_image = cv2.rectangle(rgb_image, (bbox.xmin, bbox.ymax), (bbox.xmax, bbox.ymin), (0, 0, 0), 2) 
+    plt.imshow(rgb_image)
     plt.show()        
     plt.imshow(cropped_image)
     plt.show() 
-    segmap = get_segmap_from_bbox(image, bbox)
+    segmap = get_segmap_from_bbox(rgb_image, bbox)
+    plt.imshow(segmap)
+    plt.show()    
+    segmap = get_segmap_from_bbox_with_depth(rgb_image,depth_image, bbox)
     plt.imshow(segmap)
     plt.show()    
     
--- robot_toolkit/test_megapose6d.py
+++ robot_toolkit/test_megapose6d.py
@@ -18,29 +18,10 @@ def main():
     rs_camera = RealSenseCamera()
     object_name = "powerdrill"
     obj_dir_path = "robot_arm_algos/src/inference/objects/"+object_name
-    # try:
-    #     object_mesh = o3d.io.read_triangle_mesh(obj_dir_path + "/meshes/" + object_name + "/textured.obj")
-    # except: 
-    #     object_mesh = o3d.io.read_point_cloud(obj_dir_path + "/meshes/" + object_name + "/textured.obj")
-        
-    # o3d.visualization.draw_geometries([object_mesh], point_show_normal=False)
-
+    
     megapose = MegaPose6D(object_name = object_name,
                           obj_dir_path = obj_dir_path)
-    # megapose = MegaPose6D(object_name = "soupcan",
-    #                       obj_dir_path = "robot_arm_algos/src/inference/objects/soupcan")    
-    # plt.imshow(rs_camera.get_current_rgb_frame())
-    # plt.show(block=False)
-    # plt.pause(4.0)
-    # plt.close()
-    start = timeit.timeit()
-    logger.info("hello")
-
     rvec, tvec, output = megapose.estimate_pose(rs_camera)
-    end = timeit.timeit()
-    logger.info(end - start)    
-    print(output.poses[0])
-    print(output.poses_input[0])
     rvec1, tvec1 = rvectvec_from_tf(output.poses[0].cpu().detach().numpy())
     rvec2, tvec2 = rvectvec_from_tf(output.poses_input[0].cpu().detach().numpy())
     rgb_image, depth_image = rs_camera.get_current_rgbd_frames()
@@ -48,16 +29,10 @@ def main():
     
     rgb_image = draw_estimated_frame(rgb_image, rs_camera, rvec1, tvec1)
     
-    # new_image = draw_estimated_frame(new_image, rs_camera, rvec1, tvec1)
     plt.imshow(rgb_image)
     plt.show(block=True)
 
-    # object_mesh = o3d.io.read_triangle_mesh(obj_dir_path + "/meshes/" + object_name + "/textured.obj")
 
-    # object_mesh.transform(output.poses[0].cpu().detach().numpy())
-    
-    # frame_mesh = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2).transform(output.poses[0].cpu().detach().numpy())
-    # o3d.visualization.draw_geometries([pcd, frame_mesh, object_mesh], point_show_normal=False)
     
 if __name__ == "__main__":
     main()     
