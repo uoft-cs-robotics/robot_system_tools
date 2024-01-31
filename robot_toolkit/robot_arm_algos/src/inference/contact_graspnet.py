3b38586039f96807236060a7e1798e16e8272188
Ruthrash <ruthrash.hari@gmail.com>
2024-01-31 11:40:00 -0500

    added contactgraspnet class and testscript

--- robot_toolkit/robot_arm_algos/src/inference/contact_graspnet.py
+++ robot_toolkit/robot_arm_algos/src/inference/contact_graspnet.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2
+import timeit
 # Tensorflow
 
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
+import matplotlib.pyplot as plt
 import sys 
 sys.path.insert(0, '/root/git/contact_graspnet/contact_graspnet')
  
@@ -15,8 +16,9 @@ from data import regularize_pc_point_count, depth2pc, load_available_input_data
 from contact_grasp_estimator import GraspEstimator
 from visualization_utils import visualize_grasps, show_image
 
-from ..camera.camera import get_bbox_annotations, get_segmap_from_bbox
+from ..camera.camera import get_bbox_annotations, get_segmap_from_bbox_with_depth , get_segmap_from_bbox
 from ._grasp_predictor import GraspPredictor
+from ..logger import logger
 class ContactGraspNet(GraspPredictor):#(object)
     def __init__(self,
                 global_config,
@@ -47,11 +49,18 @@ class ContactGraspNet(GraspPredictor):#(object)
         
         pass
 
-    def generate_grasps(self, rgbd_camera):
+    def generate_grasps(self, rgbd_camera, is_visualize_grasps = False, use_depth_for_seg = False):
         rgb_image, depth_image = rgbd_camera.get_current_rgbd_frames()
         bbox, _ = get_bbox_annotations(rgb_image)
-        segmap = get_segmap_from_bbox(rgb_image, bbox)
+        if use_depth_for_seg:
+            segmap = get_segmap_from_bbox_with_depth(rgb_image, depth_image, bbox)
+        else:            
+            segmap = get_segmap_from_bbox(rgb_image, bbox)        
+        # 
+        plt.imshow(segmap)
+        plt.show()    
         cam_k = rgbd_camera.camera_matrix
+        start = timeit.timeit()
         pc_full, pc_segments, pc_colors = self.grasp_estimator.extract_point_clouds(depth_image, 
                                                                                     cam_k, 
                                                                                     segmap=segmap,
@@ -63,10 +72,15 @@ class ContactGraspNet(GraspPredictor):#(object)
                                                                                             local_regions=self.local_regions, 
                                                                                             filter_grasps=self.filter_grasps, 
                                                                                             forward_passes=self.forward_passes)  
+        end = timeit.timeit()
+        logger.info(f"elapsed time {end-start}")
  
         show_image(rgb_image, segmap)
-        visualize_grasps(pc_full, pred_grasps_cam, scores, plot_opencv_cam=True, pc_colors=pc_colors)
-        
+        plt.imshow(segmap)
+        plt.show()
+        if is_visualize_grasps:
+            visualize_grasps(pc_full, pred_grasps_cam, scores, plot_opencv_cam=True, pc_colors=pc_colors)
+        return pred_grasps_cam, scores, contact_pts#, _
         # get_segmap 
         # rgb image 
         # depth image 
